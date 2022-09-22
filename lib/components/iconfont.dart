980b8b7f69e9b767c6992e8c1af62b984870f6f6
apple <apple@appledeMacBook-Air.local>
2022-09-22 14:51:49 +0800

    iconfont

--- lib/components/iconfont.dart
+++ lib/components/iconfont.dart
@@ -5,4 +5,8 @@ class IconFont {
   IconFont._();
   static const IconData icon_fenxiang = IconData(0xe600, fontFamily: _family);
   static const IconData icon_tongzhi = IconData(0xe60a, fontFamily: _family);
+  static const IconData icon_tongzhi_copy =
+      IconData(0xe60b, fontFamily: _family);
+  static const IconData icon_fenxiang_copy =
+      IconData(0xe60c, fontFamily: _family);
 }
